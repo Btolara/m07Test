048cdaf54ce373dfadd426443ac9109fad118e57
Carlos Lara <55560917+Btolara@users.noreply.github.com>
2020-01-26 11:46:46 +0100

    ChangeTItle

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Carlos</h1>
+      <h1>Hello People</h1>
 
       <h2>I dont want to do magic just to be public!</h2>
       <p>More info</p>
